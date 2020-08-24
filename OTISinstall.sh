6ecf78615b169eca94d69948205a21c567897495
onetrueinstallscript <70171623+onetrueinstallscript@users.noreply.github.com>
2020-08-24 17:18:10 -0400

    Update OTISinstall.sh

--- OTISinstall.sh
+++ OTISinstall.sh
@@ -20,15 +20,12 @@ if [ $drivetype == "UEFI" ]; then
 	parted $drive mkpart primary ext4 513MiB 20GiB
 	parted $drive mkpart primary linux-swap 20GiB 24GiB
 	parted $drive mkpart primary ext4 24GiB 100%
-elif [ $drivetype == "BIOS" ]; then
+else
 	parted $drive mklabel msdos
 	parted $drive mkpart primary ext4 1MiB 20GiB
 	parted $drive set 1 boot on
 	parted $drive mkpart primary linux-swap 20GiB 24GiB
 	parted $drive mkpart primary ext4 24GiB 100%
-else
-	echo "Invalid option selected, please try again."
-	partition $drive
 fi
 }
 
@@ -43,14 +40,11 @@ if [ drivetype == "UEFI" ]; then
 	mkfs.ext4 -F $drive\4
 	mkswap $drive\3
 	swapon $drive\3
-elif [ $drivetype == "BIOS" ]; then
+else
 	mkfs.ext4 -F $drive\1
 	mkswap $drive\2
 	swapon $drive\2
 	mkfs.ext4 -F $drive\3
-else
-	echo "Invalid option selected, please try again."
-	format $drive
 fi
 }
 
@@ -65,16 +59,13 @@ if [ $drivetype == "UEFI" ]; then
 	mkdir /mnt/home
 	mount $drive\1 /mnt/boot
 	mount $drive\4 /mnt/home
-elif [ $drivetype == "BIOS" ]; then
+else
 	echo "Mounting /dev/sda1 to /mnt"
 	mount $drive\1 /mnt
 	echo "Creating directory /mnt/home"
 	mkdir /mnt/home
 	echo "Mounting /dev/sda3 to /mnt/home"
 	mount $drive\3 /mnt/home
-else
-	echo "Invalid option selected, please try again."
-	mount $drive
 fi
 }
 
