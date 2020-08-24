b9fe980c87b54d8a6a84c7e798f5ba66b938ae85
onetrueinstallscript <70171623+onetrueinstallscript@users.noreply.github.com>
2020-08-24 17:16:47 -0400

    Update OTISinstall.sh

--- OTISinstall.sh
+++ OTISinstall.sh
@@ -13,14 +13,14 @@ echo "Drivetype (UEFI or BIOS):"
 read drivetype
 
 partition() {
-if [ "$drivetype" == "UEFI" ]; then
+if [ $drivetype == "UEFI" ]; then
 	parted $drive mklabel gpt
 	parted $drive mkpart ESP fat32 1MiB 513MiB
 	parted $drive set 1 boot on
 	parted $drive mkpart primary ext4 513MiB 20GiB
 	parted $drive mkpart primary linux-swap 20GiB 24GiB
 	parted $drive mkpart primary ext4 24GiB 100%
-elif [ "$drivetype" == "BIOS" ]; then
+elif [ $drivetype == "BIOS" ]; then
 	parted $drive mklabel msdos
 	parted $drive mkpart primary ext4 1MiB 20GiB
 	parted $drive set 1 boot on
@@ -37,13 +37,13 @@ echo "Partitioning finished."
 
 # Format the partitions
 format() {
-if [ "$drivetype" == "UEFI" ]; then
+if [ drivetype == "UEFI" ]; then
 	mkfs.vfat -F32 $drive\1
 	mkfs.ext4 -F $drive\2
 	mkfs.ext4 -F $drive\4
 	mkswap $drive\3
 	swapon $drive\3
-elif [ "$drivetype" == "BIOS" ]; then
+elif [ $drivetype == "BIOS" ]; then
 	mkfs.ext4 -F $drive\1
 	mkswap $drive\2
 	swapon $drive\2
@@ -59,13 +59,13 @@ echo "Formatting finished."
 
 # Mount the file systems
 mount() {
-if [ "$drivetype" == "UEFI" ]; then
+if [ $drivetype == "UEFI" ]; then
 	mount $drive\2 /mnt
 	mkdir /mnt/boot
 	mkdir /mnt/home
 	mount $drive\1 /mnt/boot
 	mount $drive\4 /mnt/home
-elif [ "$drivetype" == "BIOS" ]; then
+elif [ $drivetype == "BIOS" ]; then
 	echo "Mounting /dev/sda1 to /mnt"
 	mount $drive\1 /mnt
 	echo "Creating directory /mnt/home"
@@ -74,7 +74,7 @@ elif [ "$drivetype" == "BIOS" ]; then
 	mount $drive\3 /mnt/home
 else
 	echo "Invalid option selected, please try again."
-#	mount $drive
+	mount $drive
 fi
 }
 
